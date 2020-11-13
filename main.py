7d343bfbe86f1c6288178b8511f09de65baf2765
Philipp Mulfinger <info@phil-splash.de>
2020-11-13 17:14:00 +0100

    New Commit

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ with open("score_list.txt", "r") as score_file:
     score_list = json.loads(score_file.read())
     print("Top scores: " + str(score_list))
 
-    new_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
+    #sorted_score_list = sorted(score_list.items(), key=lambda x: x['attempts'])[:3]
+    #for i in sort_orders:
+    #   print(i[0], i[1])
 
     for score_dict in score_list:
-        score_text = "Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}".format(
-            score_dict.get("player_name"),
+        #sorted_score_list = sorted(score_list.items(), key=lambda x: x['attempts'])[:3]
+        sorted_score_text = "Player {0} had {1} attempts on {2}. The secret number was {3}. The wrong guesses were: {4}".format(
+            score_dict.get("player"),
             str(score_dict.get("attempts")),
             score_dict.get("date"),
             score_dict.get("secret_number"),
             score_dict.get("wrong_guesses"))
-        print(score_text)
+        print(sorted_score_text)
 
 print("_______________________START_______________________")
 player_name = input("Type your name: ")
@@ -45,6 +48,6 @@ while True:
     elif guess < secret:
         print("Your guess is not correct... try something bigger")
 
-    wrong_guesses.append(guess)
+
 
 
